15cd0e140f6c16b87c2b66d11ff4a0ba7fef97e8
MsibiD <156788159+MsibiD@users.noreply.github.com>
2024-02-23 11:56:35 +0200

    Add files via upload

---
+++ src/Temperature.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function Temperature(props) {
+  return (
+    <div class="row ">
+      <div className="col-9 main-temp">{Math.round(props.celcius)}</div>
+      <div className="col-3 unit">
+        <div className="row celcius">°C</div>
+      </div>
+    </div>
+  );
+}
